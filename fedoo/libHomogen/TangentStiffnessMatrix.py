b9e5b1afb1b5e8c2a0940302e07317c1953e4e43
Yves Chemisky <yves.chemisky@gmail.com>
2021-11-22 13:28:05 +0100

    Update TangentStiffnessMatrix.py

--- fedoo/libHomogen/TangentStiffnessMatrix.py
+++ fedoo/libHomogen/TangentStiffnessMatrix.py
@@ -1,18 +1,106 @@
 #derive de ConstitutiveLaw
 #This law should be used with an InternalForce WeakForm
 
-# from fedoo.libMesh import MeshBase as Mesh
+from simcoon import simmit as sim
+from fedoo.libConstitutiveLaw import Simcoon, ElasticIsotrop
+from fedoo.libMesh import MeshBase as Mesh
 # from fedoo.libConstitutiveLaw import ElasticIsotrop
-# from fedoo.libWeakForm import InternalForce
-# from fedoo.libAssembly import Assembly
-from fedoo.libProblem import Problem, BoundaryCondition
+from fedoo.libWeakForm import InternalForce
+from fedoo.libAssembly import Assembly
+from fedoo.libProblem import Problem, Static, BoundaryCondition
 from fedoo.libProblem import ProblemBase
 from fedoo.libUtil import DefinePeriodicBoundaryCondition
 import numpy as np
 import os
 import time
 
-def GetHomogenizedStiffness(ProblemID = None):
+def GetHomogenizedStiffness(mesh, umat_name, props, nstatev):
+    #################### PERTURBATION METHODE #############################
+
+    #Definition of the set of nodes for boundary conditions
+    if isinstance(mesh, str):
+        mesh = Mesh.GetAll()[mesh]
+
+    crd = mesh.GetNodeCoordinates()[:-2]
+    type_el = mesh.GetElementShape()
+    xmax = np.max(crd[:,0]) ; xmin = np.min(crd[:,0])
+    ymax = np.max(crd[:,1]) ; ymin = np.min(crd[:,1])
+    zmax = np.max(crd[:,2]) ; zmin = np.min(crd[:,2])
+    crd_center = (np.array([xmin, ymin, zmin]) + np.array([xmax, ymax, zmax]))/2
+    center = [np.linalg.norm(crd-crd_center,axis=1).argmin()]
+        
+    BC_perturb = np.eye(6)
+    # BC_perturb[3:6,3:6] *= 2 #2xEXY
+
+    DStrain = []
+    DStress = []
+
+    StrainNodes = mesh.AddNodes(crd_center,2) #add virtual nodes for macro strain
+
+    ElasticIsotrop(200e3, 0.3, ID = 'ElasticLaw')
+        
+    #Assembly
+    InternalForce("ElasticLaw")
+    Assembly("ElasticLaw", mesh, type_el, ID="Assembling")
+
+    #Type of problem
+    pb = Static("Assembling")
+
+    pb_post_tt = Problem(0,0,0, mesh, ID = "_perturbation")
+    pb_post_tt.SetA(pb.GetA())
+    DefinePeriodicBoundaryCondition(mesh,
+                                    [StrainNodes[0], StrainNodes[0], StrainNodes[0],
+                                     StrainNodes[1], StrainNodes[1], StrainNodes[1]],
+                                    ['DispX',        'DispY',        'DispZ',       'DispX',         'DispY',        'DispZ'], dim='3D')
+
+    BoundaryCondition('Dirichlet', 'DispX', 0, center, ID = 'center')
+    BoundaryCondition('Dirichlet', 'DispY', 0, center, ID = 'center')
+    BoundaryCondition('Dirichlet', 'DispZ', 0, center, ID = 'center')
+
+    pb_post_tt.ApplyBoundaryCondition()
+
+    DofFree = pb_post_tt._Problem__DofFree
+    MatCB = pb_post_tt._Problem__MatCB
+
+    typeBC = 'Dirichlet'
+    # typeBC = 'Neumann'
+    for i in range(6):
+        BoundaryCondition.RemoveID("_Strain")
+        BoundaryCondition(typeBC, 'DispX',
+              BC_perturb[i][0], [StrainNodes[0]], initialValue=0, ID = '_Strain')  # EpsXX
+        BoundaryCondition(typeBC, 'DispY',
+              BC_perturb[i][1], [StrainNodes[0]], initialValue=0, ID = '_Strain')  # EpsYY
+        BoundaryCondition(typeBC, 'DispZ',
+              BC_perturb[i][2], [StrainNodes[0]], initialValue=0, ID = '_Strain')  # EpsZZ
+        BoundaryCondition(typeBC, 'DispX',
+              BC_perturb[i][3], [StrainNodes[1]], initialValue=0, ID = '_Strain')  # EpsXY
+        BoundaryCondition(typeBC, 'DispY',
+              BC_perturb[i][4], [StrainNodes[1]], initialValue=0, ID = '_Strain')  # EpsXZ
+        BoundaryCondition(typeBC, 'DispZ',
+              BC_perturb[i][5], [StrainNodes[1]], initialValue=0, ID = '_Strain')  # EpsYZ
+        
+        pb_post_tt.ApplyBoundaryCondition()
+
+        pb_post_tt.Solve()
+        X = pb_post_tt.GetX()  # alias
+        DStrain.append(np.array([pb_post_tt._GetVectorComponent(X, 'DispX')[StrainNodes[0]], pb_post_tt._GetVectorComponent(X, 'DispY')[StrainNodes[0]], pb_post_tt._GetVectorComponent(X, 'DispZ')[StrainNodes[0]],
+                                  pb_post_tt._GetVectorComponent(X, 'DispX')[StrainNodes[1]], pb_post_tt._GetVectorComponent(X, 'DispY')[StrainNodes[1]], pb_post_tt._GetVectorComponent(X, 'DispZ')[StrainNodes[1]]]))
+
+        F = MatCB.T @ pb_post_tt.GetA() @ MatCB @ pb_post_tt.GetX()[DofFree]
+
+        F = F.reshape(3, -1)
+        stress = [F[0, -2], F[1, -2], F[2, -2], F[0, -1], F[1, -1], F[2, -1]]
+
+        DStress.append(stress)
+
+    if typeBC == "Neumann":
+        C = np.linalg.inv(np.array(DStrain).T)
+    else:
+        C = np.array(DStress).T
+        
+    return C
+
+def GetTangentStiffness(ProblemID = None):
     #################### PERTURBATION METHODE #############################
     if ProblemID is None: ProblemID = ProblemBase.GetActive().GetID()
     pb = ProblemBase.GetAll()[ProblemID]
