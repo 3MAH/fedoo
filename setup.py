4ca1aed106f0c3cbfd26c450d5a644bf2d404476
Kevin Marchais <kevinmarchais@gmail.com>
2021-12-16 17:11:52 +0100

    Added conda packaging files

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='fedoo',
+      version='1.0',
+      description='Finite element library including PGD solver',
+      author='3MAH',
+      url='https://github.com/3MAH/fedoo',
+      packages=['fedoo'],
+     )
