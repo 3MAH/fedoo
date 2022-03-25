e696419c7ca8693525bc595d4087b15289aee788
Yves Chemisky <yves.chemisky@gmail.com>
2022-03-25 16:01:32 +0100

    Add example to get Stiffness

---
+++ example/get_stiffness/getL.py
@@ -0,0 +1,28 @@
+from fedoo import *
+import numpy as np
+import time
+from simcoon import simmit as sim
+import os
+
+#--------------- Pre-Treatment --------------------------------------------------------
+Util.ProblemDimension("3D")
+
+Mesh.ImportFromFile('octet_surf.msh', meshID = "Domain")
+
+meshID = "Domain2"
+
+umat_name = 'ELISO'
+props = np.array([[1e5, 0.3, 1]])
+nstatev = 1
+
+L = sim.L_iso(1e5, 0.3, 'Enu')
+props_test = sim.L_iso_props(L)
+print('props', props_test)
+
+L_eff = Homogen.GetHomogenizedStiffness(meshID, L, meshperio=True)
+
+np.set_printoptions(precision=3, suppress=True)
+print('L_eff = ', L_eff)
+
+props_test_eff = sim.L_iso_props(L_eff)
+print('props', props_test_eff)
