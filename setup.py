ac5dd0a879a2ef22969ab4325038ef5f1539590a
Kevin Marchais <kevinmarchais@gmail.com>
2022-04-06 17:33:23 +0200

    build and test workflow

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='fedoo',
+      version='1.0',
+      description='Microstructure generation',
+      author='3MAH',
+      url='https://github.com/3MAH/fedoo',
+      packages=['fedoo'])
