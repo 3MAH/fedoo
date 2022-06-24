d47d4eb64d0590c140324037c981493ec087d1b0
pruliere <etienne.pruliere@ensam.eu>
2022-06-24 15:38:41 +0200

    Change output variable name

--- fedoo/libProblem/Output.py
+++ fedoo/libProblem/Output.py
@@ -12,18 +12,39 @@ from fedoo.libUtil.ExportData import ExportData
 
 
 
-_available_output = ['pkii',   'pk2',   'kirchoff',   'kirchhoff',   'cauchy',
-                      'pkii_vm','pk2_vm','krichoff_vm','kirchhoff_vm','cauchy_vm',
-                      'pkii_pc', 'pk2_pc', 'kirchoff_pc', 'kirchhoff_pc', 'cauchy_pc', 'stress_pc',
-                      'pkii_pdir1', 'pk2_pdir1', 'kirchoff_pdir1', 'kirchhoff_pdir1', 'cauchy_pdir1', 'stress_pdir1',
-                      'pkii_pdir2', 'pk2_pdir2', 'kirchoff_pdir2', 'kirchhoff_pdir2', 'cauchy_pdir2', 'stress_pdir2',
-                      'pkii_pdir3', 'pk2_pdir3', 'kirchoff_pdir3', 'kirchhoff_pdir3', 'cauchy_pdir3', 'stress_pdir3',
-                      'disp', 'rot', 'temp', 'strain', 'statev', 'stress', 'stress_vm', 'external_force', 
-                      'wm', 'internal_force', 'internal_force_global']
+# _available_output = ['pkii',   'pk2',   'kirchoff',   'kirchhoff',   'cauchy',
+#                      'pkii_vm','pk2_vm','krichoff_vm','kirchhoff_vm','cauchy_vm',
+#                      'pkii_pc', 'pk2_pc', 'kirchoff_pc', 'kirchhoff_pc', 'cauchy_pc', 'stress_pc',
+#                      'pkii_pdir1', 'pk2_pdir1', 'kirchoff_pdir1', 'kirchhoff_pdir1', 'cauchy_pdir1', 'stress_pdir1',
+#                      'pkii_pdir2', 'pk2_pdir2', 'kirchoff_pdir2', 'kirchhoff_pdir2', 'cauchy_pdir2', 'stress_pdir2',
+#                      'pkii_pdir3', 'pk2_pdir3', 'kirchoff_pdir3', 'kirchhoff_pdir3', 'cauchy_pdir3', 'stress_pdir3',
+#                      'disp', 'rot', 'temp', 'strain', 'statev', 'stress', 'stress_vm', 'fext', 
+#                      'wm', 'fint', 'fint_global']
+
+_available_output = ['PKII',   'PK2',   'Kirchoff',   'Kirchhoff',   'Cauchy',
+                     'PKII_vm','PK2_vm','Krichoff_vm','Kirchhoff_vm','Cauchy_vm',
+                     'PKII_pc', 'PK2_pc', 'Kirchoff_pc', 'Kirchhoff_pc', 'Cauchy_pc', 'Stress_pc',
+                     'PKII_pdir1', 'PK2_pdir1', 'Kirchoff_pdir1', 'Kirchhoff_pdir1', 'Cauchy_pdir1', 'Stress_pdir1',
+                     'PKII_pdir2', 'PK2_pdir2', 'Kirchoff_pdir2', 'Kirchhoff_pdir2', 'Cauchy_pdir2', 'Stress_pdir2',
+                     'PKII_pdir3', 'PK2_pdir3', 'Kirchoff_pdir3', 'Kirchhoff_pdir3', 'Cauchy_pdir3', 'Stress_pdir3',
+                     'Disp', 'Rot', 'Temp', 'Strain', 'Statev', 'Stress', 'Stress_vm', 'Fext', 
+                     'Wm', 'Fint', 'Fint_global']
+
 _available_format = ['vtk', 'msh', 'txt', 'npy', 'npz', 'npz_compressed']
 
-_label_dict = {'pkii':'PKII', 'pk2':'PKII', 'kirchoff':'Kirchhoff', 'kirchhoff':'Kirchhoff', 'cauchy':'Cauchy',
-              'stress':'Stress', 'strain':'Strain'} #use to get label associated with some outputs
+_label_dict = {'pkii':'PKII',   'pk2':'PK2',   'kirchoff':'Kirchhoff', 'kirchhoff':'Kirchhoff', 'cauchy':'Cauchy',
+                'pkii_vm':'PKII_vm','pk2_vm':'PK2_vm','kirchoff_vm':'Kirchhoff_vm','kirchhoff_vm':'Kirchhoff_vm','cauchy_vm':'Cauchy_vm',
+                'pkii_pc':'PKII_pc', 'pk2_pc':'PK2_pc', 'kirchoff_pc':'Kirchhoff_pc', 'kirchhoff_pc':'Kirchhoff_pc', 'cauchy_pc':'Cauchy_pc', 'stress_pc':'Stress_pc',
+                'pkii_pdir1':'PKII_pdir1', 'pk2_pdir1':'PK2_pdir1', 'kirchoff_pdir1':'Kirchhoff_pdir1', 'kirchhoff_pdir1':'Kirchhoff_pdir1', 'cauchy_pdir1':'Cauchy_pdir1', 'stress_pdir1':'Stress_pdir1',
+                'pkii_pdir2':'PKII_pdir2', 'pk2_pdir2':'PK2_pdir2', 'kirchoff_pdir2':'Kirchhoff_pdir2', 'kirchhoff_pdir2':'Kirchhoff_pdir2', 'cauchy_pdir2':'Cauchy_pdir2', 'stress_pdir2':'Stress_pdir2',
+                'pkii_pdir3':'PKII_pdir3', 'pk2_pdir3':'PK2_pdir3', 'kirchoff_pdir3':'Kirchhoff_pdir3', 'kirchhoff_pdir3':'Kirchhoff_pdir3', 'cauchy_pdir3':'Cauchy_pdir1', 'stress_pdir3':'Stress_pdir3',
+                'disp':'Disp', 'rot':'Rot', 'temp':'Temp', 'strain':'Strain', 'statev':'Statev', 'stress':'Stress', 'stress_vm':'Stress_vm', 'fext':'Fext', 
+                'wm':'Wm', 'fint':'Fint', 'fint_global':'Fint_global' 
+}
+
+
+#  {'pkii':'PKII', 'pk2':'PKII', 'kirchoff':'Kirchhoff', 'kirchhoff':'Kirchhoff', 'cauchy':'Cauchy',
+# 'stress':'Stress', 'strain':'Strain', 'disp':'Disp', 'rot':'Rot'} #use to get label associated with some outputs
 
 def _GetResults(pb, assemb, output_list, output_type='Node', position = 1, res_format = None):
         
@@ -35,7 +56,7 @@ def _GetResults(pb, assemb, output_list, output_type='Node', position = 1, res_f
         else: raise NameError("output_type should be either 'Node', 'Element' or 'GaussPoint'")
                 
         for i,res in enumerate(output_list):
-            output_list[i] = res = res.lower()
+            output_list[i] = _label_dict[res.lower()] #to allow full lower case str as output
             if res not in _available_output:
                 print("WARNING: '", res, "' doens't match to any available output")
                 print("Specified output ignored")
@@ -59,23 +80,21 @@ def _GetResults(pb, assemb, output_list, output_type='Node', position = 1, res_f
         
         result = {}
                     
-        for res in output_list:
-            res = res.lower()
-                                
-            if res in ['pkii', 'pk2', 'kirchoff', 'kirchhoff', 'cauchy','strain', 'stress']:
+        for res in output_list:                                
+            if res in ['PKII', 'PK2', 'Kirchhoff', 'Cauchy','Strain', 'Stress']:
                 if res in data_sav: 
                     data = data_sav[res] #avoid a new data conversion
                 else:
-                    if res in ['pkii','pk2']:
+                    if res in ['PKII','PK2']:
                         data = material.GetPKII()
-                    elif res in ['stress']:
+                    elif res == 'Stress':
                         #stress for small displacement
                         data = material.GetStress(position = position)
-                    elif res in ['kirchoff','kirchhoff']:
+                    elif res == 'Kirchhoff':
                         data = material.GetKirchhoff()
-                    elif res == 'cauchy':
+                    elif res == 'Cauchy':
                         data = material.GetCauchy()
-                    elif res == 'strain':
+                    elif res == 'Strain':
                         data = material.GetStrain(position = position)                                                
                     
                     data = data.Convert(assemb, None, output_type)                        
@@ -83,129 +102,115 @@ def _GetResults(pb, assemb, output_list, output_type='Node', position = 1, res_f
                     #keep data in memory in case it may be used later for vm, pc or pdir stress computation
                     data_sav[res] = data
                 
-                label_data = _label_dict[res]
                 if res_format == 'vtk': data = data.vtkFormat()
                 elif res_format in ['msh', 'txt']: data = np.array(data).T
                 elif res_format in ['npy', 'npz']: data = np.array(data)
                                         
-            elif res == 'disp':
+            elif res == 'Disp':
                 if output_type == 'Node':
                     if res_format in ['vtk', 'msh', 'txt']:
                         data = pb.GetDisp().T
                     else: 
                         data = pb.GetDisp()
-                    label_data = 'Disp'
                 else: 
                     raise NameError("Displacement is only a Node data and is incompatible with the output format specified")                    
 
-            elif res == 'rot':
+            elif res == 'Rot':
                 if output_type == 'Node': 
                     if res_format in ['vtk', 'msh', 'txt']:                            
                         data = pb.GetRot().T
                     else: 
                         data = pb.GetRot()
-                    label_data = 'Rot'
                 else: 
                     raise NameError("Displacement is only a Node data and is incompatible with the output format specified")                    
             
-            elif res == 'temp':
+            elif res == 'Temp':
                 if output_type == 'Node': 
                     data = pb.GetTemp().T
-                    label_data = 'Temp'
                 else: 
                     raise NameError("Temperature is only a Node data and is incompatible with the output format specified")                    
                 
-            elif res == 'external_force':
+            elif res == 'Fext':
                 if output_type == 'Node':                             
                     data = assemb.GetExternalForces(pb.GetDoFSolution())
-                    label_data = 'External_Load'                        
                 else: 
                     raise NameError("External_Force is only Node data and is incompatible with the specified output format")    
 
-            elif res in ['pkii_vm', 'pk2_vm', 'kirchoff_vm', 'kirchhoff_vm', 'cauchy_vm', 'stress_vm']:
+            elif res in ['PKII_vm', 'PK2_vm', 'Kirchhoff_vm', 'Cauchy_vm', 'Stress_vm']:
                 if res[:-3] in data_sav: 
                     data=data_sav[res[:-3]]
                 else:
-                    if res in ['pkii_vm','pk2_vm']:
+                    if res in ['PKII_vm','PK2_vm']:
                         data = material.GetPKII()
-                    elif res in ['stress_vm']:
+                    elif res == 'Stress_vm':
                         data = material.GetStress(position = position)
-                    elif res in ['kirchoff_vm','kirchhoff_vm']:
+                    elif res == 'Kirchhoff_vm':
                         data = material.GetKirchhoff()
-                    elif res == 'cauchy_vm':
+                    elif res == 'Cauchy_vm':
                         data = material.GetCauchy()
 
                     data = assemb.ConvertData(data, None, output_type)
                     data_sav[res[:-3]] = data
                                             
-                label_data = _label_dict[res[:-3]] + '_Mises'
                 data = data.vonMises()
                     
-            elif res in ['pkii_pc', 'pk2_pc', 'kirchoff_pc', 'kirchhoff_pc', 'cauchy_pc', 'stress_pc', 
-                         'pkii_pdir1', 'pk2_pdir1', 'kirchoff_pdir1', 'kirchhoff_pdir1', 'cauchy_pdir1', 'stress_pdir1',
-                         'pkii_pdir2', 'pk2_pdir2', 'kirchoff_pdir2', 'kirchhoff_pdir2', 'cauchy_pdir2', 'stress_pdir2',
-                         'pkii_pdir3', 'pk2_pdir3', 'kirchoff_pdir3', 'kirchhoff_pdir3', 'cauchy_pdir3', 'stress_pdir3']:
+            elif res in ['PKII_pc', 'PK2_pc', 'Kirchhoff_pc', 'Cauchy_pc', 'Stress_pc', 
+                         'PKII_pdir1', 'PK2_pdir1', 'Kirchhoff_pdir1', 'Cauchy_pdir1', 'Stress_pdir1',
+                         'PKII_pdir2', 'PK2_pdir2', 'Kirchhoff_pdir2', 'Cauchy_pdir2', 'Stress_pdir2',
+                         'PKII_pdir3', 'PK2_pdir3', 'Kirchhoff_pdir3', 'Cauchy_pdir3', 'Stress_pdir3']:
                 #stress principal component
                 if res[-3:] == '_pc': measure_type = res[:-3]
                 else: measure_type = res[:-6]
                 
-                if  measure_type+'_principal' in data_sav:
-                    data = data_sav[measure_type+'_principal']
+                if  measure_type+'_pc' in data_sav:
+                    data = data_sav[measure_type+'_pc']
                     
                 elif measure_type in data_sav: 
                     data = data_sav[measure_type]
                     data = data.GetPrincipalStress()
-                    data_sav[measure_type+'_principal'] = data
+                    data_sav[measure_type+'_pc'] = data
                     
                 else:                    
-                    if measure_type in ['pkii','pk2']:
+                    if measure_type in ['PKII','PK2']:
                         data = material.GetPKII()
-                    elif measure_type in ['stress']:
+                    elif measure_type == 'Stress':
                         data = material.GetStress(position = position)
-                    elif measure_type in ['kirchoff','kirchhoff']:
+                    elif measure_type == 'Kirchhoff':
                         data = material.GetKirchhoff()
-                    elif measure_type == 'cauchy':
+                    elif measure_type == 'Cauchy':
                         data = material.GetCauchy()
                     
                     data = data.Convert(assemb, None, output_type)            
                     data_sav[measure_type] = data                        
                     data = data.GetPrincipalStress()
-                    data_sav[measure_type+'_principal'] = data
+                    data_sav[measure_type+'_pc'] = data
                 
                 if res[-3:] == '_pc': #principal component
-                    data = data[0] #principal component
-                    label_data = _label_dict[measure_type] + '_Principal'
+                    data = data[0] #principal component                    
                 elif res[-6:] == '_pdir1': #1st principal direction    
-                    data = data[1][0]
-                    label_data = _label_dict[measure_type] + '_PrincipalDir1'
+                    data = data[1][0]                    
                 elif res[-6:] == '_pdir2': #2nd principal direction    
-                    data = data[1][2]
-                    label_data = _label_dict[measure_type] + '_PrincipalDir2'
+                    data = data[1][1]                    
                 elif res[-6:] == '_pdir3': #3rd principal direction    
-                    data = data[1][2]
-                    label_data = _label_dict[measure_type] + '_PrincipalDir3'
+                    data = data[1][2]                    
                     
-            elif res in ['statev']:
+            elif res == 'Statev':
                 data = material.GetStatev().T                    
                 data = assemb.ConvertData(data, None, output_type)
-                label_data = 'State_Variables'
             
-            elif res in ['wm']:
+            elif res in ['Wm']:
                 data = material.GetWm().T                    
                 data = assemb.ConvertData(data, None, output_type)
-                label_data = 'Wm'
             
-            elif res == 'internal_force':
+            elif res == 'Fint':
                 data = assemb.GetInternalForces(pb.GetDoFSolution(), 'local')
                 data = assemb.ConvertData(data, None, output_type)
-                label_data = 'Internal_Load_localCoord'   
                 
-            elif res == 'internal_force_global':
+            elif res == 'Fint_global':
                 data = assemb.GetInternalForces(pb.GetDoFSolution(), 'global')
                 data = assemb.ConvertData(data, None, output_type)
-                label_data = 'Internal_Load_globalCoord' 
             
-            result[label_data] = data
+            result[res] = data
         
         return result
         
@@ -229,7 +234,7 @@ class _ProblemOutput:
             print("List of available file format: ", _available_format)
                 
         for i,res in enumerate(output_list):
-            output_list[i] = res = res.lower()
+            output_list[i] = _label_dict[res.lower()] #to allow full lower case str as output
             if res not in _available_output:
                 print("WARNING: '", res, "' doens't match to any available output")
                 print("Specified output ignored")
