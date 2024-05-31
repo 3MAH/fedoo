87dfee30cd390f1f197e72f7dc50bba88165f5b6
Kevin Marchais <kevinmarchais@gmail.com>
2024-05-31 18:16:16 +0200

    revert undesired changes

--- tests/test_octet.py
+++ tests/test_octet.py
@@ -1,28 +1,26 @@
-from pathlib import Path
-
-import numpy as np
-
 import fedoo as fd
+import numpy as np
+ 
+fd.Assembly.delete_memory()
 
+#Define the Modeling Space - Here 3D problem 
 fd.ModelingSpace("3D")
 
-# Import the mesh generated with Microgen
-fd.mesh.import_file(
-    str(Path(__file__).parent / "../util/meshes/octet_truss.msh"), name="Domain"
-)
+#Import the mesh generated with Microgen
+fd.mesh.import_file('../util/meshes/octet_truss.msh', name = "Domain")
 
-# Get the imported mesh
+#Get the imported mesh 
 mesh = fd.Mesh["Domain2"]
 
-# Get the bounding box (corners coordinates and center)
+#Get the bounding box (corners coordinates and center)
 bounds = mesh.bounding_box
 volume = bounds.volume
 crd_center = bounds.center
-# Nearest node to the center of the bounding box for boundary conditions
+#Nearest node to the center of the bounding box for boundary conditions
 center = mesh.nearest_node(crd_center)
 
-# Add 2 virtual nodes for macro strain
-StrainNodes = fd.Mesh["Domain2"].add_virtual_nodes(crd_center, 2)
+# Add 2 virtual nodes for macro strain 
+StrainNodes = fd.Mesh["Domain2"].add_virtual_nodes(crd_center, 2)  
 
 # Material definition and simcoon elasto-plastic constitutive law
 Re = 300
@@ -30,17 +28,17 @@ k = 1000
 m = 0.25
 alpha = 1e-5
 props = np.array([1e5, 0.3, alpha, Re, k, m])
-material = fd.constitutivelaw.Simcoon("EPICP", props, name="ConstitutiveLaw")
+material = fd.constitutivelaw.Simcoon("EPICP", props, name='ConstitutiveLaw')
 
-# Create the weak formulation of the mechanical equilibrium equation
-wf = fd.weakform.StressEquilibrium("ConstitutiveLaw", name="WeakForm", nlgeom=False)
+#Create the weak formulation of the mechanical equilibrium equation
+wf = fd.weakform.StressEquilibrium("ConstitutiveLaw", name = "WeakForm", nlgeom=False)
 
 # Assembly
-assemb = fd.Assembly.create("WeakForm", "Domain2", "tet4", name="Assembly")
+assemb = fd.Assembly.create("WeakForm", "Domain2", 'tet4', name="Assembly")
 
 # Type of problem
 pb = fd.problem.NonLinear("Assembly")
-pb.set_nr_criterion(criterion="Work")
+pb.set_nr_criterion(criterion = "Work")
 
 # Set the desired ouputs at each time step
 # Problem.add_output('results', 'Assembly', ['disp', 'cauchy', 'PKII', 'strain', 'cauchy_vm', 'statev'], output_type='Node', file_format ='vtk')
@@ -48,16 +46,10 @@ pb.set_nr_criterion(criterion="Work")
 # Boundary conditions for the linearized strain tensor
 E = [0, 0, 0, 0.1, 0, 0]  # [EXX, EYY, EZZ, EXY, EXZ, EYZ]
 
-list_strain_nodes = [
-    StrainNodes[0],
-    StrainNodes[0],
-    StrainNodes[0],
-    StrainNodes[1],
-    StrainNodes[1],
-    StrainNodes[1],
-]
-list_strain_var = ["DispX", "DispY", "DispZ", "DispX", "DispY", "DispZ"]
-# or equivalent:
+list_strain_nodes = [StrainNodes[0], StrainNodes[0], StrainNodes[0],
+                     StrainNodes[1], StrainNodes[1], StrainNodes[1]]
+list_strain_var = ['DispX', 'DispY', 'DispZ','DispX', 'DispY', 'DispZ']
+# or equivalent: 
 # list_strain_nodes = [[StrainNodes[0], StrainNodes[1], StrainNodes[1]],
 #                      [StrainNodes[1], StrainNodes[0], StrainNodes[1]],
 #                      [StrainNodes[1], StrainNodes[1], StrainNodes[0]]]
@@ -65,19 +57,15 @@ list_strain_var = ["DispX", "DispY", "DispZ", "DispX", "DispY", "DispZ"]
 #                    ['DispX', 'DispY', 'DispZ'],
 #                    ['DispY', 'DispZ', 'DispZ']]
 
-bc_periodic = fd.constraint.PeriodicBC(list_strain_nodes, list_strain_var, dim=3)
+bc_periodic = fd.constraint.PeriodicBC(list_strain_nodes, list_strain_var, dim=3) 
 pb.bc.add(bc_periodic)
 
-# fixed point on the center to avoid rigid body motion
-pb.bc.add("Dirichlet", center, "Disp", 0)
+#fixed point on the center to avoid rigid body motion
+pb.bc.add('Dirichlet', center, 'Disp', 0)
 
-# Enforced mean strain
-pb.bc.add(
-    "Dirichlet", [StrainNodes[0]], "Disp", [E[0], E[1], E[2]]
-)  # EpsXX, EpsYY, EpsZZ
-pb.bc.add(
-    "Dirichlet", [StrainNodes[1]], "Disp", [E[3], E[4], E[5]]
-)  # EpsXY, EpsXZ, EpsYZ
+#Enforced mean strain
+pb.bc.add('Dirichlet', [StrainNodes[0]], 'Disp', [E[0], E[1], E[2]])  # EpsXX, EpsYY, EpsZZ
+pb.bc.add('Dirichlet', [StrainNodes[1]], 'Disp', [E[3], E[4], E[5]])  # EpsXY, EpsXZ, EpsYZ
 
 pb.apply_boundary_conditions()
 
@@ -87,9 +75,10 @@ pb.nlsolve(dt=0.2, tmax=1, update_dt=False, tol_nr=0.1)
 
 # --------------- Post-Treatment -----------------------------------------------
 # Get the stress and strain tensor (PG values)
-res = pb.get_results("Assembly", ["Strain", "Stress"], "GaussPoint")
-TensorStrain = res.gausspoint_data["Strain"]
-TensorStress = res.gausspoint_data["Stress"]
+res = pb.get_results('Assembly', ['Strain','Stress'], 'GaussPoint') 
+TensorStrain = res.gausspoint_data['Strain']
+TensorStress = res.gausspoint_data['Stress']
 
-assert np.abs(TensorStress[4][222] - 72.3765265291865) < 1e-3
-assert np.abs(TensorStrain[2][876] - 0.03046909551762696) < 1e-6
+assert np.abs(TensorStress[4][222]-72.3765265291865) <1e-3
+assert np.abs(TensorStrain[2][876]-0.03046909551762696) <1e-6
+
--- tests/test_thermal3D.py
+++ tests/test_thermal3D.py
@@ -1,67 +1,54 @@
-import os
-from pathlib import Path
-
+import fedoo as fd
 import numpy as np
+import os
 
-import fedoo as fd
-import fedoo as fd
-
-# --------------- Pre-Treatment --------------------------------------------------------
+#--------------- Pre-Treatment --------------------------------------------------------
 
 fd.ModelingSpace("3D")
 
 meshname = "Domain"
 nb_iter = 3
 
-# Mesh.box_mesh(Nx=3, Ny=3, Nz=3, x_min=0, x_max=1, y_min=0, y_max=1, z_min=0, z_max=1, ElementShape = 'hex8', name = meshname)
+# Mesh.box_mesh(Nx=3, Ny=3, Nz=3, x_min=0, x_max=1, y_min=0, y_max=1, z_min=0, z_max=1, ElementShape = 'hex8', name = meshname) 
 # Mesh.import_file('octet_surf.msh', meshname = "Domain")
 # Mesh.import_file('data/octet_1.msh', meshname = "Domain")
-mesh_file = Path(__file__).parent / "../util/meshes/gyroid.msh"
-fd.mesh.import_file(str(mesh_file), name="Domain")
+fd.mesh.import_file('../util/meshes/gyroid.msh', name = "Domain")
 
 mesh = fd.Mesh[meshname]
 
-crd = mesh.nodes
+crd = mesh.nodes 
 
-K = 500  # K = 18 #W/K/m
-c = 0.500  # J/kg/K
-rho = 7800  # kg/m2
-material = fd.constitutivelaw.ThermalProperties(K, c, rho, name="ThermalLaw")
+K = 500 # K = 18 #W/K/m
+c = 0.500 #J/kg/K
+rho = 7800 #kg/m2
+material = fd.constitutivelaw.ThermalProperties(K, c, rho, name='ThermalLaw')
 wf = fd.weakform.HeatEquation("ThermalLaw")
-assemb = fd.Assembly.create("ThermalLaw", meshname, name="Assembling")
+assemb = fd.Assembly.create("ThermalLaw", meshname, name="Assembling")    
 
-# note set for boundary conditions
+#note set for boundary conditions
 Xmin, Xmax = mesh.bounding_box
-bottom = mesh.find_nodes("Z", Xmin[2])
-top = mesh.find_nodes("Z", Xmax[2])
-left = mesh.find_nodes("X", Xmin[2])
-right = mesh.find_nodes("X", Xmax[2])
+bottom = mesh.find_nodes('Z', Xmin[2])
+top = mesh.find_nodes('Z', Xmax[2])
+left = mesh.find_nodes('X', Xmin[2])
+right = mesh.find_nodes('X', Xmax[2])
 
 pb = fd.problem.NonLinear("Assembling")
 
 # Problem.set_solver('cg', precond = True)
 
-pb.set_nr_criterion("Displacement", tol=5e-2, max_subiter=5, err0=100)
-if not (os.path.isdir("results")):
-    os.mkdir("results")
-results = pb.add_output(
-    "results/thermal3D", "Assembling", ["Temp"], output_type="Node", file_format="npz"
-)
-# Problem.add_output('results/bendingPlastic', 'Assembling', ['cauchy', 'PKII', 'strain', 'cauchy_vm', 'statev'], output_type='Element', file_format ='vtk')
+pb.set_nr_criterion("Displacement", tol = 5e-2, max_subiter=5, err0 = 100)
+if not(os.path.isdir('results')): os.mkdir('results')
+results = pb.add_output('results/thermal3D', 'Assembling', ['Temp'], output_type='Node', file_format ='npz')    
+# Problem.add_output('results/bendingPlastic', 'Assembling', ['cauchy', 'PKII', 'strain', 'cauchy_vm', 'statev'], output_type='Element', file_format ='vtk')    
 
 tmax = 10
-
-
 # Problem.bc.add('Dirichlet','Temp',0,bottom)
-def time_func(t_fact):
-    if t_fact == 0:
-        return 0
-    else:
-        return 1
-
+def time_func(t_fact): 
+    if t_fact== 0: return 0
+    else: return 1
 
 # Problem.bc.add('Dirichlet','Temp',100,left, timeEvolution=timeEvolution)
-pb.bc.add("Dirichlet", right, "Temp", 3, time_func=time_func)
+pb.bc.add('Dirichlet',right, 'Temp',3, time_func=time_func)
 # Problem.bc.add('Dirichlet','Temp',100,top, timeEvolution=timeEvolution)
 
 
@@ -69,8 +56,9 @@ pb.bc.add("Dirichlet", right, "Temp", 3, time_func=time_func)
 # Problem.bc.add('Dirichlet','DispY',0,nodes_bottomRight)
 # bc = Problem.bc.add('Dirichlet','DispY', uimp, nodes_topCenter)
 
-pb.nlsolve(dt=tmax / nb_iter, tmax=tmax, update_dt=True)
+pb.nlsolve(dt = tmax/nb_iter, tmax = tmax, update_dt = True)
 
 
 results.load()
-assert np.abs(results.node_data["Temp"][8712] - 2.610859332847924) < 1e-8
+assert np.abs(results.node_data['Temp'][8712]-2.610859332847924) < 1e-8
+
