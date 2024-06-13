0a0f8abc64059754a25218e3e29c2ee07a598556
Kevin Marchais <kevinmarchais@gmail.com>
2024-06-13 17:32:39 +0200

    fix star import in homogen subpackage

--- fedoo/homogen/__init__.py
+++ fedoo/homogen/__init__.py
@@ -1,6 +1,17 @@
-from .Homog_path import *
+"""Homogen module for homogenization of composite materials."""
+
+from .Homog_path import Read_outputfile, SolverUnitCell, get_resultsUnitCell
 from .tangent_stiffness import (
     get_homogenized_stiffness,
     get_homogenized_stiffness_2,
     get_tangent_stiffness,
 )
+
+__all__ = [
+    "Read_outputfile",
+    "SolverUnitCell",
+    "get_resultsUnitCell",
+    "get_homogenized_stiffness",
+    "get_homogenized_stiffness_2",
+    "get_tangent_stiffness",
+]
