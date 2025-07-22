904f23600f786d86a26bbce18852f8882e423112
pruliere <etienne.pruliere@ensam.eu>
2025-07-22 15:11:17 +0200

    change example

--- examples/01-simple/lattice_beam.py
+++ examples/01-simple/lattice_beam.py
@@ -9,9 +9,9 @@ import numpy as np
 E = 1e5
 nu = 0.3
 radius=1
-k = 0.9  # reduce section. k=0 to neglect the shear effect
-n = 10  # n_nodes
-ndim=3 
+k = 0 # reduce section. k=0 to neglect the shear effect
+n = 6 # n_nodes
+ndim=3
 if ndim==2:
     fd.ModelingSpace("2D")
 else:
@@ -21,25 +21,39 @@ material = fd.constitutivelaw.ElasticIsotrop(E, nu)
 beam_props = fd.constitutivelaw.BeamCircular(material, radius, k=k)
 
 # Extract a Lattice Mesh from a 3D mesh
-mesh = fd.mesh.extract_edges(fd.mesh.box_mesh(n,n,n))
-# mesh = fd.mesh.extract_edges(fd.mesh.rectangle_mesh(n,n, ndim=ndim))
+if ndim == 2:
+    mesh = fd.mesh.extract_edges(fd.mesh.rectangle_mesh(n,n, x_max=100, y_max=100, ndim=ndim))
+else:
+    mesh = fd.mesh.extract_edges(fd.mesh.box_mesh(n,n,n, x_max=100, y_max=100, z_max=100))
 
 nodes_left = mesh.find_nodes("X", mesh.bounding_box.xmin)
 nodes_right = mesh.find_nodes("X", mesh.bounding_box.xmax)
 
 wf = fd.weakform.BeamEquilibrium(beam_props)
+
 # assembly = fd.Assembly.create(wf, mesh, "bernoullibeam")
 assembly = fd.Assembly.create(wf, mesh)
 
 pb = fd.problem.NonLinear(assembly, nlgeom=True)
-# results = pb.add_output("test", assembly, ["Disp", "Rot", "BeamStress", "BeamStrain"])
-results = pb.add_output("test", assembly, ["Disp", "Rot"])
+results = pb.add_output("test", assembly, ["Disp", "Rot", "BeamStress", "BeamStrain"])
 
 pb.bc.add("Dirichlet", nodes_left, ["Disp", "Rot"], 0)
-pb.bc.add("Dirichlet", nodes_right, "DispY", -1)
-# pb.set_nr_criterion(max_subiter=5, err0=None)
-pb.nlsolve(dt=0.02, print_info=1)
+# pb.bc.add("Dirichlet", nodes_right, "DispY", -200)
+pb.bc.add("Dirichlet", nodes_right, "DispY", -50)
+# pb.bc.add("Dirichlet", nodes_right, "DispX", 2000)
+# pb.bc.add("Dirichlet", np.arange(mesh.n_nodes), "DispY", 0)
+
+pb.set_nr_criterion(max_subiter=4, err0=None, tol=1e-4)
+pb.nlsolve(dt=0.02, update_dt=True, print_info=1)
 
 # Post treatment
-results.plot("Disp", 0)
-results.write_movie("lattice", "Disp", "Y", rot_azimuth=0.2)
+# results.plot("BeamStress", 0)
+# results.write_movie("lattice", "BeamStress", "X", rot_azimuth=0)
+
+
+
+results = pb.get_results(assembly, ["Disp", "Rot", "BeamStress"])
+results.plot("BeamStress", 0)
+
+# results.plot("Rot", 0, node_labels=True)
+
--- examples/03-advanced/3D_periodic_BC_off_axis.py
+++ examples/03-advanced/3D_periodic_BC_off_axis.py
@@ -1,3 +1,14 @@
+"""
+Tensile test on a Gyroid unit cell with periodic boundary condition
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The periodic mesh of the Gyriod has been generated with microgen.
+This example show how to define periodic boundary conditions and
+to load the structure with prescribed strain in local coordinate system.
+After that, the mean stress and mean strain results are extracted in local and global
+coordinate system.
+"""
+
 import fedoo as fd
 import numpy as np
 from scipy.spatial.transform import Rotation
@@ -5,10 +16,9 @@ from scipy.spatial.transform import Rotation
 # --------------- Pre-Treatment --------------------------------------------------------
 
 fd.ModelingSpace("3D")
-# mesh = fd.mesh.import_file('../../util/meshes/gyroid_per.vtk')
 mesh = fd.Mesh.read("../../util/meshes/gyroid_per.vtk")
 # mesh = fd.mesh.box_mesh(2,2,2)
-# assert (mesh.is_periodic())
+assert (mesh.is_periodic(tol=1e-3))
 
 # Material definition
 material = fd.constitutivelaw.ElasticIsotrop(1e5, 0.3)
@@ -24,8 +34,10 @@ pb = fd.problem.Linear(assembly)
 # Boundary conditions
 
 # periodic constraint in local frame
-local_frame = Rotation.from_rotvec([0,0,np.pi/4])
-bc_periodic = fd.constraint.PeriodicBC("small_strain", off_axis_rotation=local_frame, tol=1e-3)
+local_frame = Rotation.from_rotvec([0, 0, np.pi / 4])
+bc_periodic = fd.constraint.PeriodicBC(
+    "small_strain", off_axis_rotation=local_frame, tol=1e-3
+)
 pb.bc.add(bc_periodic)
 
 # block a node near the center to avoid rigid body motion
@@ -38,13 +50,16 @@ pb.bc.add("Dirichlet", "E_11", 1)
 # for BC. For instance: pb.bc.add("Dirichlet", "E_xx", 1) will have no effect.
 
 
-# --------------- Solve --------------------------------------------------------
+# --------------- Solve -----------------------------------------------------
 pb.solve()
 
 ###############################################################################
 # print the macroscopic strain tensor and stress tensor
 
-res = pb.get_results(assembly, ['LocalMeanStrain', 'MeanStrain', 'Stress'])
+res = pb.get_results(assembly, ['LocalMeanStrain', 'MeanStrain', 'Stress', 'Disp'])
+res.plot('Stress', 'XX')
+
+np.set_printoptions(3)
 
 mean_strain = res['LocalMeanStrain'].ravel()
 # or pb.get_dof_solution("LocalMeanStrain")
