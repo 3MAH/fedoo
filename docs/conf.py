54a1c8b0c51140ef7732313c7f191dedae6c4b20
pruliere <etienne.pruliere@ensam.eu>
2026-06-03 11:11:25 +0200

    Clean doc conf

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,11 @@ copyright = "2019, Etienne Prulière"
 author = "Etienne Prulière"
 master_doc = "index"
 
+# Dynamically load the version from your package source code
+import fedoo
+version = fedoo.__version__  # Short version (e.g., 1.2)
+release = fedoo.__version__  # Full version string (e.g., 1.2.3-dev)
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -69,9 +74,9 @@ html_theme = "sphinx_rtd_theme"
 html_logo = "_static/fedOOLogos_ss_fond.png"
 
 html_theme_options = {
-    # 'canonical_url': 'https://microgen.readthedocs.io/en/latest/',
-    # 'logo_only': False,
-    # 'display_version': True,
+    # 'canonical_url': 'https://3mah.github.io/fedoo-docs/',
+    'logo_only': True,
+    'display_version': True,
     # 'prev_next_buttons_location': 'bottom',
     "style_external_links": True,
     # 'vcs_pageview_mode': '',
