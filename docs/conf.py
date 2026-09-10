086a2e42b3155db8f210b48331c022e0393fec3a
pruliere <etienne.pruliere@ensam.eu>
2026-09-10 16:41:37 +0200

    clean doc sidebar from objects

--- docs/conf.py
+++ docs/conf.py
@@ -115,6 +115,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 # -- Options for sphinx gallery for examples automatic generation ----------------------
 pyvista.OFF_SCREEN = True
